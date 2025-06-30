92cc634f62842b4e713b6aec185092f87b65b923
suvlim <suvanna0393@gmail.com>
2025-07-01 01:51:09 +0700

    Penambahan XML Bottom Navbar di Dashboard dan Pengaturan Lahan

--- app/src/main/java/com/uph23/edu/sahabattani/DashboardActivity.java
+++ app/src/main/java/com/uph23/edu/sahabattani/DashboardActivity.java
@@ -1,5 +1,6 @@
 package com.uph23.edu.sahabattani;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,6 +9,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class DashboardActivity extends AppCompatActivity {
 
     @Override
@@ -15,10 +18,24 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+//        bottomNav.setSelectedItemId(R.id.navigation_home); // tandai aktif
+//
+//        bottomNav.setOnItemSelectedListener(item -> {
+//            switch (item.getItemId()) {
+//                case R.id.navigation_home:
+//                    return true; // sudah di halaman ini
+//
+//                case R.id.navigation_lahan:
+//                    startActivity(new Intent(getApplicationContext(), PengaturanLahan.class));
+//                    overridePendingTransition(0, 0);
+//                    return true;
+//                case R.id.navigation_statistik:
+//                    startActivity(new Intent(getApplicationContext(), StatistikActivity.class));
+//                    overridePendingTransition(0, 0);
+//                    return true;
+//            }
+//            return false;
+//        });
     }
 }
